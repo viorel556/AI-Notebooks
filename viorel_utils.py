54972847175897758231ce02e9bdeb85105b286f
viorel556 <viorel.harabaru@gmail.com>
2024-08-29 15:14:01 +0300

    C#

--- viorel_utils.py
+++ viorel_utils.py
@@ -1,6 +1,22 @@
+import numpy as np 
 
 def see(*args, **kwargs):
   """Shorthand for print() to see stuff faster"""
   print(*args, **kwargs)
 
+def sigmoid(z):
+  """
+  Compute the sigmoid of z
+  Parameters
+  ----------
+  z : array_like
+      A scalar or numpy array of any size.
+  Returns
+  -------
+    g : array_like
+        sigmoid(z)
+  """
+  z = np.clip( z, -500, 500 )           # protect against overflow
+  g = 1.0/(1.0+np.exp(-z))
   
+  return g
